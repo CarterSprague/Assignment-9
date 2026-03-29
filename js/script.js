5d6f540ef33d27b340085bb86acc9e912cb12358
CarterSprague <carter.sprague@lyon.edu>
2026-03-29 12:34:50 -0500

    Fixed file path at bottom of index.html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log("script.js loaded");
+const apiKey = "lm7LQqTdRPg8NdXyvrIhncpReOhu89FJ";
+
+const gifContainer = document.querySelector("#gif-container");
+const fetchGifBtn = document.querySelector("#fetch-gif-btn");
+
+async function fetchGifs() {
+  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=cats&limit=12&rating=g`;
+
+  try {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    const images = data.data.map((gif) => gif.images.original.url);
+    console.log(images);
+
+    gifContainer.innerHTML = "";
+
+    for (const imageUrl of images) {
+      gifContainer.innerHTML += `
+        <img src="${imageUrl}" class="col-3 mb-3">
+      `;
+    }
+  } catch (error) {
+    console.error("Error fetching gifs:", error);
+  }
+}
+
+fetchGifBtn.addEventListener("click", fetchGifs);
